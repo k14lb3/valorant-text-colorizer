7f9dd6b12ddc01bea08c794776e59854863eb9d3
Kialbe <k14lb3@gmail.com>
2021-06-13 20:37:56 +0800

    Add InputText component

---
+++ components/InputText.js
@@ -0,0 +1,14 @@
+import { forwardRef } from 'react';
+
+const InputText = ({ className, ...rest }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className="p-4 bg-pampas border-2 border-pampas rounded text-2xl outline-none duration-200 focus:border-black"
+      {...rest}
+      type="text"
+    />
+  );
+};
+
+export default forwardRef(InputText);
--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
   return (
     <div className="h-screen">
-      <header className="py-4 px-8 bg-black border-b border-red">
+      <header className="py-4 px-8 bg-black border-b-2 border-red">
         <h1 className="text-red">Valorant Text Colorizer</h1>
       </header>
       <main className="h-full bg-pampas pt-5">{children}</main>
