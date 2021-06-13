197b8daa01bb035a7590f5331b45bb5b66b27703
Kialbe <k14lb3@gmail.com>
2021-06-13 22:24:01 +0800

    Add className props

--- components/InputText.js
+++ components/InputText.js
@@ -4,7 +4,9 @@ const InputText = ({ className, ...rest }, ref) => {
   return (
     <input
       ref={ref}
-      className="p-4 bg-pampas border-2 border-pampas rounded text-2xl outline-none duration-200 focus:border-black"
+      className={`p-4 bg-pampas border-2 border-pampas rounded text-2xl outline-none duration-200 focus:border-black${
+        className ? ` ${className}` : ''
+      }`}
       {...rest}
       type="text"
     />
