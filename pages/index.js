fe2dae9015bb4447a28ec92eabb1059a7910e2da
Kialbe <k14lb3@gmail.com>
2021-06-13 22:35:02 +0800

    Add stuffs lol

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
+import { useRef } from 'react';
 import Head from 'next/head';
 import Label from 'components/Label';
 import InputText from 'components/InputText';
+import Button from 'components/Button';
 
 const Home = () => {
+  const textRef = useRef();
+
+  const generateText = () => {};
+
   return (
     <>
       <Head>
         <title>Valorant Text Colorizer</title>
       </Head>
-      <div className="flex flex-col w-96 p-8 mx-auto border-2 border-black bg-white rounded">
-        <Label className="mb-5">Input text</Label>
-        <InputText />
+      <div className="flex flex-col w-192 py-8 px-12 mx-auto border-2 border-black bg-white rounded">
+        <Label className="mb-5 ">Input text</Label>
+        <InputText ref={textRef} className="mb-5" />
+        <Button className="w-1/2 mx-auto" onClick={generateText}>
+          Generate
+        </Button>
       </div>
     </>
   );
