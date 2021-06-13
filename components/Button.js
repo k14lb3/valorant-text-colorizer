3427c14cddfe864d131f024dba88052942255be3
Kialbe <k14lb3@gmail.com>
2021-06-13 22:04:31 +0800

    Add hover effect

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,13 @@
 const Button = ({ className, children, ...rest }) => {
   return (
     <button
-      className={`py-4 px-8 bg-red rounded-tl rounded-br text-pampas font-openSans text-2xl hover:bg-ebony duration-200${
+      className={`group relative py-4 px-8 bg-red rounded-tl rounded-br text-pampas font-openSans text-2xl overflow-hidden duration-200${
         className ? ` ${className}` : ''
       }`}
       {...rest}
     >
-      {children}
+      <span className="relative z-10">{children}</span>
+      <span className="absolute w-full-1/4 h-full bg-ebony top-0 -left-full-1/4 rounded-br-full duration-500 z-0 group-hover:left-0"></span>
     </button>
   );
 };
