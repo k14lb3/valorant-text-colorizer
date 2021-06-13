566c3020ff85728bd0ba5325884f47e7cf9a0e79
Kialbe <k14lb3@gmail.com>
2021-06-14 01:07:54 +0800

    Refractor styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
 const Layout = ({ children }) => {
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-pampas">
       <header className="py-4 px-8 bg-black border-b-2 border-red">
         <h1 className="text-red">Valorant Text Colorizer</h1>
       </header>
-      <main className="h-full bg-pampas pt-5">{children}</main>
+      <main className="pt-5">{children}</main>
     </div>
   );
 };
