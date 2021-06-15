772b328aced75f26d62fad291203be3224da3f03
Kialbe <k14lb3@gmail.com>
2021-06-15 23:02:25 +0800

    Add bottom padding

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
       <header className="py-4 px-8 bg-black border-b-2 border-red">
         <h1 className="text-red">Valorant Text Colorizer</h1>
       </header>
-      <main className="flex justify-center pt-5">{children}</main>
+      <main className="flex justify-center py-5">{children}</main>
     </div>
   );
 };
